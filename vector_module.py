9bfe644a34e2d3fdbeae95b1f4ebd71ca611711c
gtm1235 <40638970+gtm1235@users.noreply.github.com>
2018-10-30 14:23:30 -0700

    Update vector_module.py

---
+++ vector_module.py
@@ -0,0 +1,33 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be non empty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iteratable')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def __add__(self,v):
+        add_vector = []
+        for i in range(self.dimension):
+           add_vector.append(self.coordinates[i] + v.coordinates[i])
+        return tuple(add_vector)
+
+
+my_vector = Vector([1,2,3])
+print(my_vector)
+my_vector2 = Vector([1,2,3])
+print(my_vector2)
+print(my_vector + my_vector2)
